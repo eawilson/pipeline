bf92eca3956482d07164053cc1957632fd0e4227
Ed Wilson <edwardadrianwilson@yahoo.co.uk>
2019-11-23 17:40:26 +0000

    Work on main pipeline.

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,2 +1,3 @@
-from .utils import run, mount_basespace, mount_instance_storage, unmount_basespace, list_basespace_fastqs, ungzip_and_combine_illumina_fastqs, load_panel_from_s3, s3_put
+from .utils import run, mount_basespace, mount_instance_storage, unmount_basespace, list_basespace_fastqs, ungzip_and_combine_illumina_fastqs, \
+                    load_panel_from_s3, s3_put, illumina_readgroup
 from .cpipeline import dedup
