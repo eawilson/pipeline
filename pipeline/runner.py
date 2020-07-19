96f52dc5056038b26b24268b11d40d90f94dd407
Ubuntu <ubuntu@ip-172-31-1-74.eu-west-2.compute.internal>
2020-07-19 17:34:13 +0000

    Debugged runner.py

--- pipeline/runner.py
+++ pipeline/runner.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
-import tarfile
 import json
 import boto3
 import requests
+import pdb
+import itertools
 
-from pipeline import mount_instance_storage
+from pipeline import mount_instance_storage, am_i_an_ec2_instance
 
 
     
@@ -17,7 +18,7 @@ def terminate():
 
 
 
-def parse_url(self, url):
+def parse_url(url):
     if url[:5].lower() != ("s3://"):
         raise RuntimeError(f"{url} is ot a valid s3 url.")
     parts = url.split("/")
@@ -40,30 +41,38 @@ class S3(object):
         if path:
             fn = f"{path}/{fn}"
         if fn not in self.downloaded:
-            self.client.download_file(bucket, key, fn)
             self.downloaded.add(fn)
+            self.client.download_file(bucket, key, fn)
+            if fn.endswith(".tar.gz"):
+                archive = fn
+                fn = fn[:-7]
+                if "_vep_" in fn:
+                    fn = "vep"
+                os.makedirs(fn, exist_ok=True)
+                subprocess.run(["tar", "-xzf", archive, "-C", fn])
+                os.unlink(archive)
         return fn
     
     def upload(self, fn, url):
-        bucket, key, fn = parse_url(f"{url}/{fn}"))
+        bucket, key, fn = parse_url(f"{url}/{fn}")
         self.client.upload_file(fn, bucket, key)
         
         
 
 def main():
     if am_i_an_ec2_instance():
-        os.cwd(mount_instance_storage())
+        os.chdir(mount_instance_storage())
     
     s3 = S3()
     sqs = boto3.client("sqs")
-    queue_url = sqs.get_queue_url(QueueName="samples.fifo")["QueueUrl"]
+    queue_url = sqs.get_queue_url(QueueName="samples")["QueueUrl"]
     while True:
-        if any(os.path.isfile(fn) for fn if os.listdir()):
+        if any(os.path.isfile(fn) for fn in os.listdir()):
             raise RuntimeError("Working directory is not empty.")
         
         response = sqs.receive_message(QueueUrl=queue_url,
-                                       VisibilityTimeout=60*30,
-                                       WaitTimeSeconds=20}
+                                       VisibilityTimeout=30,
+                                       WaitTimeSeconds=20)
         
         try:
             message = response["Messages"][0]
@@ -74,24 +83,17 @@ def main():
         
         script = body["Script"]
         if script[:5].lower() == ("s3://"):
-            os.mkdirs("scripts", exist_ok=True)
+            os.makedirs("scripts", exist_ok=True)
             script = os.path.join(".", s3.download(script, path="scripts"))
         
         body["Args"] = [s3.download(url) for url in body.get("Args", ())]
         
-        for paramter, value in body.get("Kwargs", {}).items():
+        for parameter, value in body.get("Kwargs", {}).items():
             if value[:5].lower() == ("s3://"):
                 fn = s3.download(value)
-                if fn.endswith(".tar.gz"):
-                    with tarfile.open(fn, "r:gz") as archive:
-                        members = archive.getnames()
-                    top_level = [member for member in members if "/" not in member]
-                    if len(top_level) != 1:
-                        raise RuntimeError(f"{url} is not a valid tar.gz directory.")
-                    fn = top_level[0]
                 body["Kwargs"][parameter] = fn
-        
-        command_line = script + body.get("Args", []) + list(chain(*body.get("Kwargs", {}).items()))
+        pdb.set_trace()
+        command_line = [script] + body.get("Args", []) + list(inertools.chain(*body.get("Kwargs", {}).items()))
         command_line = " ".join([(f"'{token}'" if " " in token else token) for token in command_line])
         with open("{}.log.txt".format(body["Kwargs"]["--sample"]), "wb") as log:
             completed_process = subprocess.run(command_line, shell=True, stderr=log)
@@ -103,7 +105,7 @@ def main():
                 os.unlink(fn)
         
         response = sqs.delete_message(QueueUrl=queue_url,
-                                      ReceiptHandle=message["ReceiptHandle"]
+                                      ReceiptHandle=message["ReceiptHandle"])
 
 
 
