54e826c3092d8406e400769cbca8898841cc4c30
Ed Wilson <edwardadrianwilson@yahoo.co.uk>
2021-05-27 14:09:20 +0100

    Add ability to rename runs to bscopy.

--- pipeline/bscopy.py
+++ pipeline/bscopy.py
@@ -8,8 +8,12 @@ from pipeline import s3_put, s3_list
 
 
 
-def bscopy(bsruns, project, include=None, exclude=None, dry_run=False):
+def bscopy(bsruns, project, include=None, exclude=None, dry_run=False, rename_run=""):
+    if rename_run and len(bsruns) > 1:
+        sys.exit("--rename-run can only be specified if a single run is selected")
+
     for bsrun in bsruns:
+        awsrun = rename_run or bsrun
         stem = os.path.expanduser(f"~/basespace/Projects/{bsrun}/Samples")
         if not os.path.exists(stem):
             sys.exit(f"Basespace run {bsrun} does not exist")
@@ -21,7 +25,7 @@ def bscopy(bsruns, project, include=None, exclude=None, dry_run=False):
                 print(f"Skipping {sample}")
                 continue
             
-            prefix = f"projects/{project}/samples/{bsrun}/{sample}"
+            prefix = f"projects/{project}/samples/{awsrun}/{sample}"
             
             uploaded = s3_list("omdc-data", prefix)
             for fastq in glob.glob(f"{stem}/{sample}/Files/*fastq.gz"):
@@ -39,6 +43,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('bsruns', nargs="+", help="Basespace runs.")
     parser.add_argument("-p", "--project", help="AWS project.", required=True)
+    parser.add_argument("-r", "--rename-run", help="Rename run when copying to AWS..", default=argparse.SUPPRESS)
     parser.add_argument('-i', "--include", help="Only include fastqs matching this regular expression.", default=argparse.SUPPRESS)
     parser.add_argument('-e', "--exclude", help="Exclude fastqs matching this regular expression.", default=argparse.SUPPRESS)
     parser.add_argument('-d', "--dry-run", help="Do not actually upload anything to aws.", action="store_true", default=argparse.SUPPRESS)
