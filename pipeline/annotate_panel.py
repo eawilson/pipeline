7d276b3abf4a654a475c91d7272f4cc393974ef2
Ed Wilson <edwardadrianwilson@yahoo.co.uk>
2021-09-04 16:24:48 +0100

    Add annotation for non transcript consequences

--- pipeline/annotate_panel.py
+++ pipeline/annotate_panel.py
@@ -14,17 +14,19 @@ from pipeline import run, pipe
 from covermi import Panel, appris
 
 BIOTYPE = defaultdict(int, (("protein_coding", 1), ("pseudogene", -1)))
+IMPACT = {"HIGH": 3, "MODERATE": 2, "LOW": 1, "MODIFIER": 0}
 REFSEQ = defaultdict(int, (("NM", 4), ("NR", 3), ("XM", 2), ("XR", 1)))
 DELETE_NON_DIGIT = str.maketrans("", "", "ABCDEFGHIJKLMNOPQRSTUVWXYZ_")
 
 CHROM = 0
 QUAL = 5
 FILTERS = 6
-
+FMT_KEYS = 8
+FMT_VALS = 9
 
 
 def vardict_read_data(row):
-    fmt = dict(zip(row[8].split(":"), row[9].split(":")))
+    fmt = dict(zip(row[FMT_KEYS].split(":"), row[FMT_VALS].split(":")))
     ref_fr = [int(n) for n in fmt["RD"].split(",")]
     alt_fr = [int(n) for n in fmt["ALD"].split(",")]
     return {"vaf": fmt["AF"], "depth": fmt["DP"], "alt_depth": fmt["VD"], "ref_fr": ref_fr, "alt_fr": alt_fr}
@@ -32,7 +34,7 @@ def vardict_read_data(row):
 
 
 def varscan2_read_data(row):
-    fmt = dict(zip(row[8].split(":"), row[9].split(":")))
+    fmt = dict(zip(row[FMT_KEYS].split(":"), row[FMT_VALS].split(":")))
     vaf = float(fmt["FREQ"].rstrip("%"))/100
     ref_fr = [int(fmt["RDF"]), int(fmt["RDR"])]
     alt_fr = [int(fmt["ADF"]), int(fmt["ADR"])]
@@ -41,12 +43,12 @@ def varscan2_read_data(row):
 
 
 def mutect2_read_data(row):
-    fmt = dict(zip(row[8].split(":"), row[9].split(":")))
+    fmt = dict(zip(row[FMT_KEYS].split(":"), row[FMT_VALS].split(":")))
     vaf = float(fmt["AF"])
     alt_depth = fmt["AD"].split(",")[1]
     d4 = fmt["SB"].split(",")
     ref_fr = [int(n) for n in d4[:2]]
-    alt_fr = [int)n) for n in d4[2:]]
+    alt_fr = [int(n) for n in d4[2:]]
     return {"vaf": f"{vaf:.4f}", "depth": fmt["DP"], "alt_depth": alt_depth, "ref_fr": ref_fr, "alt_fr": alt_fr}
 
 
@@ -56,6 +58,46 @@ def chrom2int(chrom):
         return int(chrom[3:])
     except ValueError:
         return math.inf
+
+
+
+def parse_colocated(vep_output):
+    demographics = defaultdict(list)
+    for colocated in vep_output.get("colocated_variants", ()):
+        allele_string = colocated["allele_string"]
+        identifier = colocated["id"]
+        if allele_string == "COSMIC_MUTATION":
+            demographics["cosmic"] += [identifier]
+
+        elif allele_string == "HGMD_MUTATION":
+            demographics["hgmd"] += [identifier]
+
+        else:
+            if identifier.startswith("rs"):
+                demographics["dbsnp"] += [identifier]
+            
+            if "pubmed" in colocated:
+                pubmed = colocated["pubmed"] if isinstance(colocated["pubmed"], list) else [colocated["pubmed"]]
+                demographics["pubmed"] += [str(ref) for ref in pubmed]
+                if "clin_sig" in colocated:
+                    demographics["clin_sig"] += colocated["clin_sig"]
+                
+            frequencies = colocated.get("frequencies")
+            if frequencies:
+                frequencies = frequencies.get(vep_output["allele_string"].split("/")[1], {})
+                statistics = defaultdict(list)
+                lookup = {"gnomad_afr": "gnomad", "gnomad_amr": "gnomad", "gnomad_asj": "gnomad", "gnomad_eas": "gnomad", "gnomad_fin": "gnomad", \
+                        "gnomad_nfe": "gnomad", "gnomad_oth": "gnomad", "gnomad_sas": "gnomad", "ea": "nhlbi", "aa": "nhlbi", \
+                        "afr": "1k", "amr": "1k", "asn": "1k", "eur": "1k", "eas": "1k", "sas": "1k", "gnomad": "gnomad"}
+                for population, frequency in frequencies.items():
+                    statistics[lookup[population]] += [frequency]
+                if statistics:
+                    demographics["maf"] = max(chain(*statistics.values()))
+    
+    if "clin_sig" in demographics:
+        demographics["clin_sig"] = sorted(set(demographics["clin_sig"]))
+    
+    return demographics
 
 
 
@@ -130,7 +172,9 @@ def annotate_panel(vcf, vep, reference=None, threads=None, output="output.tsv", 
             transcript, minor = cons["transcript_id"].split(".")
             prefix = transcript[:2]
             major = transcript[3:]
-            return [BIOTYPE[cons["biotype"]],
+            return [transcript in needed_transcripts,
+                    cons["gene_symbol"] in needed_genes,
+                    BIOTYPE[cons["biotype"]],
                     REFSEQ[prefix],
                     -int(cons["gene_id"]),
                     principal.get(transcript, 0),
@@ -141,23 +185,33 @@ def annotate_panel(vcf, vep, reference=None, threads=None, output="output.tsv", 
     else: # ensembl transcripts
         def consequence_sort(cons):
             # Version numbers not in vep as of version 101, but who knows the future ...
-            return [BIOTYPE[cons["biotype"]],
+            transcript = cons["transcript_id"]
+            return [transcript in needed_transcripts,
+                    cons["gene_symbol"] in needed_genes,
+                    BIOTYPE[cons["biotype"]],
                     -int(cons["gene_id"].translate(DELETE_NON_DIGIT)),
-                    principal.get(cons["transcript_id"], 0),
+                    principal.get(transcript, 0),
                     "canonical" in cons,
-                    -int(cons["transcript_id"].translate(DELETE_NON_DIGIT))]
+                    -int(transcript.translate(DELETE_NON_DIGIT))]
     
     annotations = []
     with open(vepjson) as f:
         for line in f:
             vep_output = json.loads(line)
-
-            consequences = vep_output.get("transcript_consequences", ())
-            consequences = [cons for cons in consequences if "gene_symbol" in cons and "transcript_id" in cons]
-            if not consequences:
-                continue
             
-            # create variant - assume one variant per row ?always true
+            consequences = vep_output.get("transcript_consequences") 
+            if consequences:
+                cons = sorted(consequences, key=consequence_sort)[-1]
+                other_genes = set(c["gene_symbol"] for c in consequences) - set([cons["gene_symbol"]])
+            
+            else:
+                most_severe_consequence = vep_output["most_severe_consequence"]
+                for cons in sorted(chain(*[v for k, v in vep_output.items() if k.endswith("_consequences")]), key=lambda x:x.get("biotype", ""), reverse=True):
+                    # We are only going to use biotype and impact so probably does not matter which one we choose so long as we are consistent
+                    if most_severe_consequence in c["consequence_terms"]:
+                        break
+                other_genes = ()
+            
             row = vep_output["input"].rstrip().split("\t")
             read_data = get_read_data(row)
             
@@ -165,92 +219,37 @@ def annotate_panel(vcf, vep, reference=None, threads=None, output="output.tsv", 
                 continue
             
             # https://gatk.broadinstitute.org/hc/en-us/articles/360035532152-Fisher-s-Exact-Test
-            fisher_strand = -10 * math.log10(fisher_exact([ref_fr, alt_fr])[1])
+            fisher_strand = -10 * math.log10(fisher_exact([read_data["ref_fr"], read_data["alt_fr"]])[1])
             
-            demographics = defaultdict(list)
-            for colocated in vep_output.get("colocated_variants", ()):
-                allele_string = colocated["allele_string"]
-                identifier = colocated["id"]
-                if allele_string == "COSMIC_MUTATION":
-                    demographics["cosmic"] += [identifier]
+            demographics = parse_colocated(vep_output)
 
-                elif allele_string == "HGMD_MUTATION":
-                    demographics["hgmd"] += [identifier]
-
-                else:
-                    if identifier.startswith("rs"):
-                        demographics["dbsnp"] += [identifier]
-                    
-                    if "pubmed" in colocated:
-                        pubmed = colocated["pubmed"] if isinstance(colocated["pubmed"], list) else [colocated["pubmed"]]
-                        demographics["pubmed"] += [str(ref) for ref in pubmed]
-                        if "clin_sig" in colocated:
-                            demographics["clin_sig"] += colocated["clin_sig"]
-                        
-                    frequencies = colocated.get("frequencies")
-                    if frequencies:
-                        frequencies = frequencies.get(vep_output["allele_string"].split("/")[1], {})
-                        statistics = defaultdict(list)
-                        lookup = {"gnomad_afr": "gnomad", "gnomad_amr": "gnomad", "gnomad_asj": "gnomad", "gnomad_eas": "gnomad", "gnomad_fin": "gnomad", \
-                                "gnomad_nfe": "gnomad", "gnomad_oth": "gnomad", "gnomad_sas": "gnomad", "ea": "nhlbi", "aa": "nhlbi", \
-                                "afr": "1k", "amr": "1k", "asn": "1k", "eur": "1k", "eas": "1k", "sas": "1k", "gnomad": "gnomad"}
-                        for population, frequency in frequencies.items():
-                            statistics[lookup[population]] += [frequency]
-                        if statistics:
-                            demographics["maf"] = max(chain(*statistics.values()))
-            
-            if "clin_sig" in demographics:
-                demographics["clin_sig"] = sorted(set(demographics["clin_sig"]))
-            
-
-            all_genes = set()
-            potentials = defaultdict(list)
-            genes_with_transcripts = set()
-            for cons in consequences:
-                gene_symbol = cons["gene_symbol"]
-                all_genes.add(gene_symbol)
-                transcript_id = cons["transcript_id"].split(".")[0]
-                if transcript_id in needed_transcripts:
-                    potentials[transcript_id].append(cons)
-                    genes_with_transcripts.add(gene_symbol)
-                elif gene_symbol in needed_genes:
-                    potentials[gene_symbol].append(cons)
-            
-            if potentials:
-                selected = [sorted(val, key=consequence_sort)[-1] for key, val in potentials.items() if key not in genes_with_transcripts]
-            else:
-                selected = [sorted(consequences, key=consequence_sort)[-1]]
-            
-
-            for cons in selected:
-                gene_symbol = cons.get("gene_symbol", "")
-                annotations.append([gene_symbol,
-                                    cons.get("transcript_id", ""),
-                                    row[CHROM],
-                                    vep_output["start"],
-                                    vep_output["allele_string"],
-                                    row[QUAL],
-                                    row[FILTERS],
-                                    read_data["vaf"],
-                                    read_data["depth"],
-                                    read_data["alt_depth"],
-                                    "{}:{}".format(*read_data["alt_fr"]),
-                                    "{}{}".format(*read_data["ref_fr"]),
-                                    fisher_strand,
-                                    cons.get("hgvsc", ""),
-                                    cons.get("hgvsp", ""),
-                                    cons.get("biotype", ""),
-                                    cons.get("impact", ""),
-                                    ", ".join(demographics.get("clin_sig", ())),
-                                    ", ".join(cons.get("consequence_terms", ())),
-                                    cons.get("sift_prediction", ""),
-                                    cons.get("polyphen_prediction", ""),
-                                    "{:.10f}".format(demographics["maf"]) if "maf" in demographics else "",
-                                    ", ".join(sorted(all_genes - set([gene_symbol]))),
-                                    ", ".join(demographics.get("dbsnp", ())),
-                                    ", ".join(demographics.get("hgmd", ())),
-                                    ", ".join(demographics.get("cosmic", ())),
-                                    ", ".join(demographics.get("pubmed", ()))])
+            annotations.append([cons.get("gene_symbol", ""),
+                                cons.get("transcript_id", ""),
+                                row[CHROM],
+                                vep_output["start"],
+                                vep_output["allele_string"],
+                                row[QUAL],
+                                row[FILTERS],
+                                read_data["vaf"],
+                                read_data["depth"],
+                                read_data["alt_depth"],
+                                "{}:{}".format(*read_data["alt_fr"]),
+                                "{}:{}".format(*read_data["ref_fr"]),
+                                fisher_strand,
+                                cons.get("hgvsc", ""),
+                                cons.get("hgvsp", ""),
+                                cons.get("biotype", ""),
+                                cons.get("impact", ""),
+                                ", ".join(demographics.get("clin_sig", ())),
+                                ", ".join(cons.get("consequence_terms", ())),
+                                cons.get("sift_prediction", ""),
+                                cons.get("polyphen_prediction", ""),
+                                "{:.10f}".format(demographics["maf"]) if "maf" in demographics else "",
+                                ", ".join(sorted(other_genes)),
+                                ", ".join(demographics.get("dbsnp", ())),
+                                ", ".join(demographics.get("hgmd", ())),
+                                ", ".join(demographics.get("cosmic", ())),
+                                ", ".join(demographics.get("pubmed", ()))])
     
     
     os.unlink(vepjson)
